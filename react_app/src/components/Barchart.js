35635b749c1500dd88f524fe619663ed72d10b63
muloneweb <49344807+muloneweb@users.noreply.github.com>
2022-10-28 13:03:35 +0200

    Create Barchart.js

---
+++ react_app/src/components/Barchart.js
@@ -0,0 +1,45 @@
+
+import React from "react"
+import '../index.css'
+import { Bar } from "react-chartjs-2"
+
+export const BarChart = React.memo(({ chartData }) => {
+  
+  return (
+    <div className="graphschart" style={{  borderRadius: "5px", marginBottom: "8px"}}>
+      <Bar 
+        data={chartData}
+        options={{
+          categoryPercentage: 0.8,
+          barPercentage: 0.6,
+          scales: {
+            x: {
+              ticks: {
+                display: false,
+              }
+            },
+            y: {
+              ticks: {
+                display: false,
+              }
+            }
+          },
+          responsive: true,
+          maintainAspectRatio: false,
+          indexAxis: 'x',
+          plugins: {
+            title: {
+              display: false,
+              text: "time"
+            },
+            legend: {
+              display: false,
+              // color: "#dddddd",
+              // position: "bottom"
+            }
+          }
+        }}
+      />
+    </div>
+  );
+});
